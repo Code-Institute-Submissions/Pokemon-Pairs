d3c51c8a9f7c23db6dfbd315a074e69f68bf7227
cgpalmer <cgpalmer91@gmail.com>
2020-03-28 19:16:43 +0000

    Both pairs will now say well done when both of the pair is selected, regardless of the order

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,34 +7,45 @@ let x = 0;
 let y = 0;
 
 $("#firstCard").click(function(){
-    x++;
-    console.log(x);
-    if ( x == 2){
-        console.log("Well done!")
-    }
-});
+    if (x == 0) {
+        x++;
+        console.log(x);
+    } else if(x == 1){
+            x++;
+            console.log(x);
+            console.log("Well done!");
+        }
+    });
 
 $("#secondCard").click(function(){
-    x++;
-    console.log(x);
-    if ( x == 2){
-        console.log("Well done!")
-    }
+    if (x == 0) {
+        x++;
+        console.log(x);
+    } else if(x == 1){
+            x++;
+            console.log(x);
+            console.log("Well done!");
+        }
 });
 
 $("#thirdCard").click(function(){
-    y++;
-    console.log(y);
-    if ( y == 2){
-        console.log("Well done!")
-    }
+   if (y == 0) {
+        y++;
+        console.log(y);
+    } else if(y == 1){
+            y++;
+            console.log(y);
+            console.log("Well done!");
+        }
 });
 
 $("#fourthCard").click(function(){
-    y++;
-    console.log(y);
-    if ( y == 2){
-        console.log("Well done!")
-    }
+    if (y == 0) {
+        y++;
+        console.log(y);
+    } else if(y == 1){
+            y++;
+            console.log(y);
+            console.log("Well done!");
+        }
 });
-
