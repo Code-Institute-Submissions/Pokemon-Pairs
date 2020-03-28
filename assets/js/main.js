14d95e94c29cf1c171af41fb4c42d794e8ae09a0
cgpalmer <cgpalmer91@gmail.com>
2020-03-28 19:31:46 +0000

    JS now allows the user to click any box. If the user clicks the pair consecutively then 'well done' will be logged. If the user clicks any cards that do not belong to a pair consecutively then it will display, 'sorry not a match'

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,45 +7,61 @@ let x = 0;
 let y = 0;
 
 $("#firstCard").click(function(){
-    if (x == 0) {
-        x++;
-        console.log(x);
-    } else if(x == 1){
+    if (y == 1) {
+        console.log("Sorry not a match!");
+    }
+    else if(x == 1){
             x++;
             console.log(x);
             console.log("Well done!");
-        }
-    });
+        } 
+    else {
+        x++;
+        console.log(x);
+    }
+});
 
 $("#secondCard").click(function(){
-    if (x == 0) {
-        x++;
-        console.log(x);
-    } else if(x == 1){
+     if (y == 1) {
+        console.log("Sorry not a match!");
+    }
+    else if(x == 1){
             x++;
             console.log(x);
             console.log("Well done!");
-        }
+        } 
+    else {
+        x++;
+        console.log(x);
+    }
 });
 
 $("#thirdCard").click(function(){
-   if (y == 0) {
-        y++;
-        console.log(y);
-    } else if(y == 1){
+    if (x == 1) {
+        console.log("Sorry not a match!");
+    }
+    else if(y == 1){
             y++;
             console.log(y);
             console.log("Well done!");
-        }
+        } 
+    else {
+        y++;
+        console.log(y);
+    }
 });
 
 $("#fourthCard").click(function(){
-    if (y == 0) {
-        y++;
-        console.log(y);
-    } else if(y == 1){
+  if (x == 1) {
+        console.log("Sorry not a match!");
+    }
+    else if(y == 1){
             y++;
             console.log(y);
             console.log("Well done!");
-        }
+        } 
+    else {
+        y++;
+        console.log(y);
+    }
 });
