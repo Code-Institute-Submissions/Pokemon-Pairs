efc8e81adaa3b44fffcdab48a3c1c62205c40fe0
Chris Palmer <cgpalmer91@gmail.com>
2020-03-28 11:38:32 +0000

    Added JS to that when a card is clicked, it adds a value of 1 to a variable

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,27 @@ $(".box").click(function(){
     $(this).css("background-color", "green");
 
 });
+
+let x = 0;
+let y = 0;
+
+$("#firstCard").click(function(){
+    x++;
+    console.log(x);
+});
+
+$("#secondCard").click(function(){
+    x++;
+    console.log(x);
+});
+
+$("#thirdCard").click(function(){
+    y++;
+    console.log(y);
+});
+
+$("#fourthCard").click(function(){
+    y++;
+    console.log(y);
+});
+
