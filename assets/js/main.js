ec9e0f67382880f4862c6cc1ac5b4ea627e9e32c
cgpalmer <cgpalmer91@gmail.com>
2020-03-29 23:56:45 +0000

    Added a 'do while' loop which will pick a random value from the array.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,10 @@ $("#start").click(function(){
 let array1 = [1, 7];
 let array2 = [3, 4, 5];
 let array3 = [11, 13];
+
+do {
+let number1 = array1[Math.floor(Math.random() * 3)];
+} while(number1 == comparison);
+
+console.log(number1);
 });
