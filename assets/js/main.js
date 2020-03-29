3ee3f5ee7ae29aa77585b949160ad34d85a7582b
cgpalmer <cgpalmer91@gmail.com>
2020-03-29 23:59:11 +0000

    FB - changed let to var to be defined. Also changed *3 to *2 as there were only two values in the array1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,10 @@ let array1 = [1, 7];
 let array2 = [3, 4, 5];
 let array3 = [11, 13];
 
+let comparison = 0;
+
 do {
-let number1 = array1[Math.floor(Math.random() * 3)];
+var number1 = array1[Math.floor(Math.random() * 2)];
 } while(number1 == comparison);
 
 console.log(number1);
