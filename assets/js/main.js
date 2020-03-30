e096414edf8b49d94740503fcc3a6c3a18821874
cgpalmer <cgpalmer91@gmail.com>
2020-03-30 00:06:15 +0000

    Added extra loops to define numbers 3-5 using array2. Again with no repeats.

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,20 @@ var number2 = array1[Math.floor(Math.random() * 2)];
 
 console.log(number1);
 console.log(number2);
+
+do {
+var number3 = array2[Math.floor(Math.random() * 3)];
+} while(number3 == comparison);
+
+do {
+var number4 = array2[Math.floor(Math.random() * 3)];
+} while(number4 == comparison || number4 == number3);
+
+do {
+var number5 = array2[Math.floor(Math.random() * 3)];
+} while(number5 == comparison || number5 == number4 || number5 == number4 );
+
+console.log(number3);
+console.log(number4);
+console.log(number5);
 });
