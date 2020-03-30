5b0aa183d06c6f047b584205875ed388a316812b
cgpalmer <cgpalmer91@gmail.com>
2020-03-30 00:21:13 +0000

    Cleaned up the code removing any unnecessary loops and comparison arguments.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,6 @@ let array1 = [1, 7];
 let array2 = [3, 4, 5];
 let array3 = [11, 13];
 
-
-
-
 var number1 = array1[Math.floor(Math.random() * 2)];
 
 do {
@@ -15,30 +12,26 @@ var number2 = array1[Math.floor(Math.random() * 2)];
 console.log(number1);
 console.log(number2);
 
-// do {
-// var number3 = array2[Math.floor(Math.random() * 3)];
-// } while(number3 == comparison);
+var number3 = array2[Math.floor(Math.random() * 3)];
 
-// do {
-// var number4 = array2[Math.floor(Math.random() * 3)];
-// } while(number4 == comparison || number4 == number3);
+do {
+     var number4 = array2[Math.floor(Math.random() * 3)];
+    } while(number4 == number3);
 
-// do {
-// var number5 = array2[Math.floor(Math.random() * 3)];
-// } while(number5 == comparison || number5 == number4 || number5 == number4 );
+do {
+var number5 = array2[Math.floor(Math.random() * 3)];
+} while(number5 == number4 || number5 == number3 );
 
-// console.log(number3);
-// console.log(number4);
-// console.log(number5);
+console.log(number3);
+console.log(number4);
+console.log(number5);
 
-// do {
-// var number6 = array3[Math.floor(Math.random() * 2)];
-// } while(number6 == comparison);
+var number6 = array3[Math.floor(Math.random() * 2)];
 
-// do {
-// var number7 = array3[Math.floor(Math.random() * 2)];
-// } while(number7 == comparison || number7 == number1);
+do {
+var number7 = array3[Math.floor(Math.random() * 2)];
+} while(number7 == number6);
 
-// console.log(number6);
-// console.log(number7);
+console.log(number6);
+console.log(number7);
 });
