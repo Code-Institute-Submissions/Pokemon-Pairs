f15d62da47094f60b5bc14f2590c02b2e2e56034
cgpalmer <cgpalmer91@gmail.com>
2020-03-30 07:39:59 +0000

    Adding in the code from the master branch as I accidently deleted it and unsure if it will cause a merge error. Will create a new js script next time.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 $("#start").click(function(){
     //Declaring the arrays
 let array1 = [1, 7];
@@ -127,4 +55,104 @@ console.log(number14);
 console.log(number15);
 console.log(number16);
 console.log("The total is" + " " + test + ". These numbers are random.");
+});
+
+
+
+let x = 0;
+let y = 0;
+let z = 0;
+
+    
+$("#firstCard").click(function(){
+    if (y == 1) {
+        console.log("Sorry not a match!");
+        console.log(x);
+        $(".box").css("background-color", "blue");
+        y = 0;
+        console.log(y);
+    }
+    else if(x == 1){
+            x++;
+            console.log(x);
+            console.log("Well done!");
+            $(this).css("display", "none");
+            $("#secondCard").css("display", "none");
+    } 
+    else {
+        x++;
+        console.log(x);
+    }
+});
+
+$("#secondCard").click(function(){
+     if (y == 1) {
+        console.log("Sorry not a match!");
+        console.log(x);
+        $(".box").css("background-color", "blue");
+        y = 0;
+        console.log(y);
+    }
+    else if(x == 1){
+            x++;
+            console.log(x);
+            console.log("Well done!");
+            $(this).css("display", "none");
+            $("#firstCard").css("display", "none");
+        } 
+    else {
+        x++;
+        console.log(x);
+    }
+});
+
+$("#thirdCard").click(function(){
+    if (x == 1) {
+        console.log("Sorry not a match!");
+        console.log(y);
+      $(".box").css("background-color", "blue");
+        x = 0;
+        console.log(x);
+    }
+    else if(y == 1){
+            y++;
+            console.log(y);
+            console.log("Well done!");
+            $(this).css("display", "none");
+            $("#fourthCard").css("display", "none");
+        } 
+    else {
+        y++;
+        console.log(y);
+    }
+});
+
+$("#fourthCard").click(function(){
+   if (x == 1) {
+        console.log("Sorry not a match!");
+        console.log(y);
+      $(".box").css("background-color", "blue");
+        x = 0;
+        console.log(x);
+    }
+    else if(y == 1){
+            y++;
+            console.log(y);
+            console.log("Well done!");
+            $(this).css("display", "none");
+            $("#thirdCard").css("display", "none");
+        } 
+    else {
+        y++;
+        console.log(y);
+    }
+});
+
+$(".box").click(function(){
+    $(this).css("background-color", "green");
+    z++;
+    console.log("You have used" + " " + z + " " + "moves!")
+    if(x == 2 && y == 2){
+        console.log("Game over!");
+    }
 });
