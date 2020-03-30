244706fad189a8f775042d6018a841ede10c313e
cgpalmer <cgpalmer91@gmail.com>
2020-03-30 00:38:25 +0000

    FB - Added in number 16 to the array3 and adjusted the following code to allow for it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $("#start").click(function(){
 let array1 = [1, 7];
 let array2 = [3, 4, 5];
-let array3 = [11, 13];
+let array3 = [11, 13, 16];
 
 var number1 = array1[Math.floor(Math.random() * 2)];
 
@@ -26,29 +26,35 @@ console.log(number3);
 console.log(number4);
 console.log(number5);
 
-var number6 = array3[Math.floor(Math.random() * 2)];
+var number6 = array3[Math.floor(Math.random() * 3)];
 
 do {
-var number7 = array3[Math.floor(Math.random() * 2)];
+var number7 = array3[Math.floor(Math.random() * 3)];
 } while(number7 == number6);
+
+do {
+    var number8 = array3[Math.floor(Math.random() * 3)];
+} while(number8 == number6 || number8 == number6);
 
 console.log(number6);
 console.log(number7);
 
-let number8 = number1 * 2;
-let number9 = number2 * 2;
-let number10 = number3 * 2;
-let number11 = number4 * 2;
-let number12 = number5 * 2;
-let number13 = number3 * 3;
-let number14 = number4 * 3;
-let number15 = number5 * 3;
+let number9 = number1 * 2;
+let number10 = number2 * 2;
+let number11 = number3 * 2;
+let number12 = number4 * 2;
+let number13 = number5 * 2;
+let number14 = number3 * 3;
+let number15 = number4 * 3;
+let number16 = number5 * 3;
 
 console.log(number8);
 console.log(number9);
 console.log(number10);
 console.log(number11);
 console.log(number12);
-
-
+console.log(number13);
+console.log(number14);
+console.log(number15);
+console.log(number16);
 });
