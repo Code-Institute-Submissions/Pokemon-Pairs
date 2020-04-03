9f1680c27f1ad2c4fafb1eb9780aff294fe229f6
cgpalmer <cgpalmer91@gmail.com>
2020-04-03 21:26:19 +0000

    Linked the nav list to change the src of the iframe. They link with the html pages

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,15 @@
 $("#menuButton").click(function(){
     $("#navList").toggle("display", "none");
 });
+
+$("#linkPlay").click(function(){
+    $("#iframe").attr("src", "game.html");
+});
+
+$("#linkHelp").click(function(){
+    $("#iframe").attr("src", "help.html");
+});
+
+$("#linkContactUs").click(function(){
+    $("#iframe").attr("src", "contactUs.html");
+});
