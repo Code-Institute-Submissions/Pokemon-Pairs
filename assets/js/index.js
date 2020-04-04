42cf2ffc539bfa7f08cb8b6efd287fb34b96b5b9
cgpalmer <cgpalmer91@gmail.com>
2020-04-04 15:31:01 +0000

    Adding in the js to show the timer display

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,22 @@
+//Menu opening JS
 $("#menuButton").click(function(){
     $("#indexGrid").toggleClass("index-grid");
     $("#indexGrid").toggleClass("index-grid-menuShow");
     $("#gridItemMenu").toggle("display", "block");
 });
 
+//Game information display
+$("#gridItemResults").click(function(){
+    $("#topResultsWrapper").toggle("display", "block");
+});
 
+$("#gridItemTimer").click(function(){
+    $("#gridItemTimerDisplay").toggle("display", "block");
+});
+
+
+
+//NAV - Changing the contents of the iframe
 $("#linkPlay").click(function(){
     $("#iframe").attr("src", "game.html");
 });
