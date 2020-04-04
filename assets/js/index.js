f93a6c2a5840cc3c974a573c868123d7ce2e42d7
cgpalmer <cgpalmer91@gmail.com>
2020-04-04 15:05:44 +0000

    Adding JQuery and extra width class for iframe to get it to respond when the menu opens.

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,8 @@ $("#menuButton").click(function(){
     $("#indexGrid").toggleClass("index-grid");
     $("#indexGrid").toggleClass("index-grid-menuShow");
     $("#gridItemMenu").toggle("display", "block");
+    $("#iframe").toggleClass("iframe"); //Clean this code up before you submit.
+    $("#iframe").toggleClass("iframeMenuShow");
 });
 
 
