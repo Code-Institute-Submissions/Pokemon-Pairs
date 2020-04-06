54c026cade9e294eb6c5c3ebe96f9dd491384d66
cgpalmer <cgpalmer91@gmail.com>
2020-04-06 21:17:41 +0000

    Adding in a template literal to generate the easy game when the easy tab is clicked

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,5 @@
+
+
 //Menu opening JS
 $("#menuButton").click(function(){
     $("#indexGrid").toggleClass("index-grid");
@@ -28,3 +30,22 @@ $("#linkHelp").click(function(){
 $("#linkContactUs").click(function(){
     $("#iframe").attr("src", "contactUs.html");
 });
+
+
+
+//JS for selecting difficulty
+
+$("#easy").click(function(){
+    $("#message").html(`<button id="start">Click to start</button> 
+    <div id="card-grid" class="card-grid-container">
+        <div id="card1" class="box pair1">card1</div>
+        <div id="card2" class="box pair1">card2</div>
+        <div id="card3" class="box pair2">card3</div>
+        <div id="card4" class="box pair2">card4</div>
+        <div id="card5" class="box pair3">card5</div>
+        <div id="card6" class="box pair3">card6</div>
+        <div id="card7" class="box pair4">card7</div>
+        <div id="card8" class="box pair4">card8</div>    
+    </div>`);
+  });
+
