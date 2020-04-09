c6131610860a37899dff0ecc3d331404d2c30613
cgpalmer <cgpalmer91@gmail.com>
2020-04-09 23:13:39 +0000

    Adding in 16 cards for the medium and harder version

--- assets/js/index.js
+++ assets/js/index.js
@@ -59,7 +59,15 @@ $("#medium").click(function(){
         <div id="card5" class="box pair3">card5</div>
         <div id="card6" class="box pair3">card6</div>
         <div id="card7" class="box pair4">card7</div>
-        <div id="card8" class="box pair4">card8</div>    
+        <div id="card8" class="box pair4">card8</div>   
+        <div id="card9" class="box pair1" onclick="myfunction('card1');">card1</div>   
+        <div id="card10" class="box pair1" onclick="myfunction('card2');">card2</div>
+        <div id="card11" class="box pair2">card3</div>
+        <div id="card12" class="box pair2">card4</div>
+        <div id="card13" class="box pair3">card5</div>
+        <div id="card14" class="box pair3">card6</div>
+        <div id="card15" class="box pair4">card7</div>
+        <div id="card16" class="box pair4">card8</div> 
     </div>`);
     $("#gridItemName").addClass("display");
     $("#gridItemResults").addClass("display");
@@ -69,15 +77,23 @@ $("#medium").click(function(){
 
 $("#hard").click(function(){
     $("#game").html(`
-    <div id="card-grid" class="card-grid-container">
-        <div id="card1" class="box pair1" onclick="pair1();">card1</div>   
-        <div id="card2" class="box pair1" onclick="pair1();">card2</div>
+    <div id="card-grid" class="card-grid-container">     
+        <div id="card1" class="box pair1" onclick="myfunction('card1');">card1</div>   
+        <div id="card2" class="box pair1" onclick="myfunction('card2');">card2</div>
         <div id="card3" class="box pair2">card3</div>
         <div id="card4" class="box pair2">card4</div>
         <div id="card5" class="box pair3">card5</div>
         <div id="card6" class="box pair3">card6</div>
         <div id="card7" class="box pair4">card7</div>
-        <div id="card8" class="box pair4">card8</div>    
+        <div id="card8" class="box pair4">card8</div>   
+        <div id="card9" class="box pair1" onclick="myfunction('card1');">card1</div>   
+        <div id="card10" class="box pair1" onclick="myfunction('card2');">card2</div>
+        <div id="card11" class="box pair2">card3</div>
+        <div id="card12" class="box pair2">card4</div>
+        <div id="card13" class="box pair3">card5</div>
+        <div id="card14" class="box pair3">card6</div>
+        <div id="card15" class="box pair4">card7</div>
+        <div id="card16" class="box pair4">card8</div>     
     </div>`);
     $("#gridItemName").addClass("display");
     $("#gridItemResults").addClass("display");
