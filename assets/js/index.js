3b300a92b2856d02c1517ee5122e7733bf19f433
cgpalmer <cgpalmer91@gmail.com>
2020-04-09 15:28:51 +0000

    Adding in some js to make the divs turn colors independently and adding in a variable and trying to turn it into jquery

--- assets/js/index.js
+++ assets/js/index.js
@@ -51,44 +51,37 @@ $("#easy").click(function(){
 
    
 
-  //make sure the id and the parameter are equal. Make sure there is a paramter or the functions won't work!  
  
-
-  function myfunction(test){
-      console.log("This has worked!");
-      document.getElementById(test).style.backgroundColor = "green";
-  }; 
-
  
-// $("#medium").click(function(){
-//     $("#message").html(`<button id="start">Click to start</button> 
-//     <div id="card-grid" class="card-grid-container">
-//         <div id="card1" class="box pair1" onclick="myfunction('card1');">card1</div>   
-//         <div id="card2" class="box pair1" onclick="myfunction('card2');">card2</div>
-//         <div id="card3" class="box pair2">card3</div>
-//         <div id="card4" class="box pair2">card4</div>
-//         <div id="card5" class="box pair3">card5</div>
-//         <div id="card6" class="box pair3">card6</div>
-//         <div id="card7" class="box pair4">card7</div>
-//         <div id="card8" class="box pair4">card8</div>    
-//     </div>`);
+$("#medium").click(function(){
+    $("#message").html(`<button id="start">Click to start</button> 
+    <div id="card-grid" class="card-grid-container">
+        <div id="card1" class="box pair1" onclick="myfunction('card1');">card1</div>   
+        <div id="card2" class="box pair1" onclick="myfunction('card2');">card2</div>
+        <div id="card3" class="box pair2">card3</div>
+        <div id="card4" class="box pair2">card4</div>
+        <div id="card5" class="box pair3">card5</div>
+        <div id="card6" class="box pair3">card6</div>
+        <div id="card7" class="box pair4">card7</div>
+        <div id="card8" class="box pair4">card8</div>    
+    </div>`);
     
-//   });
+  });
 
-// $("#hard").click(function(){
-//     $("#message").html(`<button id="start">Click to start</button> 
-//     <div id="card-grid" class="card-grid-container">
-//         <div id="card1" class="box pair1" onclick="myfunction('card1');">card1</div>   
-//         <div id="card2" class="box pair1" onclick="myfunction('card2');">card2</div>
-//         <div id="card3" class="box pair2">card3</div>
-//         <div id="card4" class="box pair2">card4</div>
-//         <div id="card5" class="box pair3">card5</div>
-//         <div id="card6" class="box pair3">card6</div>
-//         <div id="card7" class="box pair4">card7</div>
-//         <div id="card8" class="box pair4">card8</div>    
-//     </div>`);
+$("#hard").click(function(){
+    $("#message").html(`<button id="start">Click to start</button> 
+    <div id="card-grid" class="card-grid-container">
+        <div id="card1" class="box pair1" onclick="pair1();">card1</div>   
+        <div id="card2" class="box pair1" onclick="pair1();">card2</div>
+        <div id="card3" class="box pair2">card3</div>
+        <div id="card4" class="box pair2">card4</div>
+        <div id="card5" class="box pair3">card5</div>
+        <div id="card6" class="box pair3">card6</div>
+        <div id="card7" class="box pair4">card7</div>
+        <div id="card8" class="box pair4">card8</div>    
+    </div>`);
     
-//   });
+  });
 
 
 
@@ -100,3 +93,16 @@ $("#easy").click(function(){
 //     $("#gameInfoShow").toggleClass("display");
 //     $("#gameInfoHide").toggleClass("display");    
 //   });
+
+
+
+
+//JS for the game
+
+ var color = "blue";
+  function pair1(){
+    //   console.log("This has worked!");
+    //   document.getElementById().style.backgroundColor = color;
+      $('#card1').css("color", "yellow");
+      $('#card1').css("background-color", color)
+  }; 
