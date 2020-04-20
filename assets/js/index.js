b23a618a276ce678ae11189ecbc4df05e4769075
cgpalmer <cgpalmer91@gmail.com>
2020-04-20 22:20:12 +0000

    Adding the function in from the tutorial.

--- assets/js/index.js
+++ assets/js/index.js
@@ -94,9 +94,19 @@ $("#easyButton").click(function(){
     console.log("this is the original array " + originalArray);
     var shuffledArray = shuffle(originalArray);
      function shuffle(array) {
-
-        
-     }
+     var currentIndex = array.length, temporaryValue, randomIndex;
+     // While there remain elements to shuffle...
+     while (0 !== currentIndex) {
+     // Pick a remaining element...
+     randomIndex = Math.floor(Math.random() * currentIndex);
+     currentIndex -= 1;
+     // And swap it with the current element.
+     temporaryValue = array[currentIndex];
+     array[currentIndex] = array[randomIndex];
+     array[randomIndex] = temporaryValue;
+    }
+    return array;                             
+    }
 
 
 
