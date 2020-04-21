5ea5e258f6e0707d552be0eb653a3e9fa4002a5f
cgpalmer <cgpalmer91@gmail.com>
2020-04-21 20:35:04 +0000

    Giving the reveal cards a timeout to ensure they are revealed properly.

--- assets/js/game.js
+++ assets/js/game.js
@@ -763,9 +763,12 @@ function card12Animation(){
  $(".card").click(function(){
         console.log("This is the amount of clicks =" + click);
     if(click == 6){
+        var revealTime = setTimeout(revealCards,1000);
+        function revealCards(){
         $("#card1, #card2, #card3, #card4, #card5, #card6, #card7, #card8, #card9, #card10, #card11, #card12").css("display", "block");
         $("#card1Match, #card2Match, #card3Match, #card4Match, #card5Match, #card6Match, #card7Match, #card8Match, #card9Match, #card10Match, #card11Match, #card12Match").addClass("display");
-        var wait = setTimeout(finishedModal, 1000);
+        }
+        var wait = setTimeout(finishedModal, 2000);
         function finishedModal(){
         $("#finishedModal").css("display", "block");
         $("#finishedModal").addClass("in");
