224918b25b757acb33577ad9ee7c648383ebcf51
cgpalmer <cgpalmer91@gmail.com>
2020-04-21 15:56:14 +0000

    Adding a timing function to show 'start game' when the array has at elast 6 cards in.

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,10 +19,20 @@ $("#enterName").click(function(){
     $("#welcomeModal").css("display", "none");
 	$("#welcomeModal").removeClass("in");
 	$("#welcomeModal").removeClass("show");
-	$("body").removeClass("modal-open");
+    $("body").removeClass("modal-open");
+    
     $("#playerName").html(playerName);
     $("#loadingModalHeader").html(`<h5 class="modal-title" id="loadingModalTitle">Welcome, ${playerName}!</h5>`);
     console.log(nameChosen);
+
+    var time= setInterval(loadingArray, 1000);
+    function loadingArray(){
+        if(test.length >= 6){
+            $("#startGame").removeClass("display");
+            clearInterval(loadingArray);
+        }
+    }
+
 });
 
 $("#startGame, #playAgainButton, #playAgainButtonDisplay").click(function(){
