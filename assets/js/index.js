a1029e60009732280a641952622b6b832bd8cd30
Chris Palmer <cgpalmer91@gmail.com>
2020-05-06 22:48:29 +0000

    Applied the image to the match card to display when the pair is found

--- assets/js/index.js
+++ assets/js/index.js
@@ -94,19 +94,10 @@ $("#seeCardsButton").click(function(){
 	$("#finishedModal").removeClass("show");
     $("#indexBody").removeClass("modal-open");
 });
-
-
-
-// //Menu opening Js
-// $("#menuButton").click(function(){
-//     $("#navList").slideToggle();
-// });
-
-  
+ 
 
 //Distributing card images.
 var cardIDArray = [1,2,3,4,5,6,7,8,9,10,11,12];
-// var pairIDArray = [1,1,2,2,3,3,4,4,5,5,6,6];
 console.log("This is the cardID array " + cardIDArray);
 
 $("#startGame").click(function(){
@@ -124,19 +115,6 @@ var shuffledCardID = shuffle(cardIDArray);
          return array;
       }
       
-    // var shuffledPairIDArray = shuffle(pairIDArray);
-    // function shuffle(array) {
-    // var currentIndex = array.length, temporaryValue, randomIndex;
-    // while (0 !== currentIndex) {
-    //      randomIndex = Math.floor(Math.random() * currentIndex);
-    //      currentIndex -= 1;
-    //      temporaryValue = array[currentIndex];
-    //      array[currentIndex] = array[randomIndex];
-    //      array[randomIndex] = temporaryValue;
-    //      }
-
-    //      return array;
-    //   }
     for(i=0; i<12; i++){
     $("#gameRow").append(`
                     <div id="card${shuffledCardID[i]}" class="col-4 col-sm-2  col-md-3 col-xl-2 eight-cards card" onclick="cardFunction${shuffledCardID[i]}();">
@@ -147,13 +125,10 @@ var shuffledCardID = shuffle(cardIDArray);
                     `
                     );
             
-            $("#card1, #card2").addClass("pair1");
-            $("#card3, #card4").addClass("pair2");
-            $("#card5, #card6").addClass("pair3");
-            $("#card7, #card8").addClass("pair4");
-            $("#card9, #card10").addClass("pair5");
-            $("#card11, #card12").addClass("pair6");
-            
+            for(j=1;j<13;j++){
+                var pairArray =["placeholder",1,1,2,2,3,3,4,4,5,5,6,6];
+                $(`#card${j}, #card${j}Match`).addClass(`pair${pairArray[j]}`);
+            }        
      
     }
     
@@ -177,64 +152,7 @@ var shuffledCardID = shuffle(cardIDArray);
 
 
     // //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    // var originalArray = ["pair1", "pair1", 
-    //                      "pair2", "pair2", 
-    //                      "pair3", "pair3", 
-    //                      "pair4", "pair4", 
-    //                      "pair5", "pair5", 
-    //                      "pair6", "pair6"];
-    // console.log("this is the original array " + originalArray);
-    // var shuffledArray = shuffle(originalArray);
-    //  function shuffle(array) {
-    //  var currentIndex = array.length, temporaryValue, randomIndex;
-    //  // While there remain elements to shuffle...
-    //  while (0 !== currentIndex) {
-    //  // Pick a remaining element...
-    //  randomIndex = Math.floor(Math.random() * currentIndex);
-    //  currentIndex -= 1;
-    //  // And swap it with the current element.
-    //  temporaryValue = array[currentIndex];
-    //  array[currentIndex] = array[randomIndex];
-    //  array[randomIndex] = temporaryValue;
-    // }
-    // return array;                             
-    // }
     
-   
-    // //End of tutorial code
-    // console.log("this is the shuffled array " + shuffledArray);
-    // //for loop all of these.
-    // $("#card1Face").addClass(shuffledArray[0]);
-    // $("#card2Face").addClass(shuffledArray[1]);
-    // $("#card3Face").addClass(shuffledArray[2]);
-    // $("#card4Face").addClass(shuffledArray[3]);
-    // $("#card5Face").addClass(shuffledArray[4]);
-    // $("#card6Face").addClass(shuffledArray[5]);
-    // $("#card7Face").addClass(shuffledArray[6]);
-    // $("#card8Face").addClass(shuffledArray[7]);
-    // $("#card9Face").addClass(shuffledArray[8]);
-    // $("#card10Face").addClass(shuffledArray[9]);
-    // $("#card11Face").addClass(shuffledArray[10]);
-    // $("#card12Face").addClass(shuffledArray[11]);
-    // $("#card1").addClass(`${shuffledArray[0]}A`);
-    // $("#card2").addClass(`${shuffledArray[1]}A`);
-    // $("#card3").addClass(`${shuffledArray[2]}A`)
-    // $("#card4").addClass(`${shuffledArray[3]}A`);
-    // $("#card5").addClass(`${shuffledArray[4]}A`);
-    // $("#card6").addClass(`${shuffledArray[5]}A`);
-    // $("#card7").addClass(`${shuffledArray[6]}A`);
-    // $("#card8").addClass(`${shuffledArray[7]}A`);
-    // $("#card9").addClass(`${shuffledArray[8]}A`);
-    // $("#card10").addClass(`${shuffledArray[9]}A`);
-    // $("#card11").addClass(`${shuffledArray[10]}A`);
-    // $("#card12").addClass(`${shuffledArray[11]}A`);
-    // var formattedShuffledArray = "'" + shuffledArray[0] + shuffledArray[1] + shuffledArray[2] + "'"
-    //                             + "'" + shuffledArray[3] +shuffledArray[4] + shuffledArray[5] + "'" 
-    //                             + "'" + shuffledArray[6] + shuffledArray[7] +shuffledArray[8] + "'"
-    //                             + "'" + shuffledArray[9] + shuffledArray[10] +shuffledArray[11] + "'" ;
-    // console.log(formattedShuffledArray);
-    // $(".card-grid-container").css("grid-template-areas", formattedShuffledArray); 
-
     $(".pair1").css("background-image", imageContainerArray[0]);
     $(".pair2").css("background-image", imageContainerArray[1]);
     $(".pair3").css("background-image", imageContainerArray[2]);
