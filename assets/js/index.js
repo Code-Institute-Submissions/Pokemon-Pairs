339d67aec8d9b0b390040edc63e21dc6c9acd247
Chris Palmer <cgpalmer91@gmail.com>
2020-05-06 20:06:42 +0000

    Commenting out unnecessary code.

--- assets/js/index.js
+++ assets/js/index.js
@@ -133,9 +133,9 @@ var shuffledCardID = shuffle(cardIDArray);
     $(".row").append(`
                     <div id="card${shuffledCardID[i]}" class="col-4 col-sm-2  col-md-3 col-xl-2 eight-cards card" onclick="firstFunction${shuffledCardID[i]}();">
                         <div id="card${shuffledCardID[i]}Cover" class=" col-4 eight-cards cover"></div> 
-                        <div id="card${shuffledCardID[i]}Face" class="col-4 col-sm-2  col-md-3 col-xl-2 eight-cards  face"></div> 
+                        <div id="card${shuffledCardID[i]}Face" class="col-4 col-sm-2  col-md-3 col-xl-2 eight-cards  pair${i} face"></div> 
                     </div>
-                    <div id="card${shuffledCardID[i]}Match" class="col-4 col-sm-2  col-md-3 col-xl-2 eight-cards match card display"></div>`
+                    <div id="card${shuffledCardID[i]}Match" class="col-4 col-sm-2  col-md-3 col-xl-2 eight-cards match card pair${i} display"></div>`
                     );
     }
     
@@ -157,58 +157,58 @@ var shuffledCardID = shuffle(cardIDArray);
                                'url('+ '"' + image6 + '"' + ')'];
 
 
-    //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    var originalArray = ["pair1", "pair1", 
-                         "pair2", "pair2", 
-                         "pair3", "pair3", 
-                         "pair4", "pair4", 
-                         "pair5", "pair5", 
-                         "pair6", "pair6"];
-    console.log("this is the original array " + originalArray);
-    var shuffledArray = shuffle(originalArray);
-     function shuffle(array) {
-     var currentIndex = array.length, temporaryValue, randomIndex;
-     // While there remain elements to shuffle...
-     while (0 !== currentIndex) {
-     // Pick a remaining element...
-     randomIndex = Math.floor(Math.random() * currentIndex);
-     currentIndex -= 1;
-     // And swap it with the current element.
-     temporaryValue = array[currentIndex];
-     array[currentIndex] = array[randomIndex];
-     array[randomIndex] = temporaryValue;
-    }
-    return array;                             
-    }
+    // //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    // var originalArray = ["pair1", "pair1", 
+    //                      "pair2", "pair2", 
+    //                      "pair3", "pair3", 
+    //                      "pair4", "pair4", 
+    //                      "pair5", "pair5", 
+    //                      "pair6", "pair6"];
+    // console.log("this is the original array " + originalArray);
+    // var shuffledArray = shuffle(originalArray);
+    //  function shuffle(array) {
+    //  var currentIndex = array.length, temporaryValue, randomIndex;
+    //  // While there remain elements to shuffle...
+    //  while (0 !== currentIndex) {
+    //  // Pick a remaining element...
+    //  randomIndex = Math.floor(Math.random() * currentIndex);
+    //  currentIndex -= 1;
+    //  // And swap it with the current element.
+    //  temporaryValue = array[currentIndex];
+    //  array[currentIndex] = array[randomIndex];
+    //  array[randomIndex] = temporaryValue;
+    // }
+    // return array;                             
+    // }
     
    
-    //End of tutorial code
-    console.log("this is the shuffled array " + shuffledArray);
-    //for loop all of these.
-    $("#card1Face").addClass(shuffledArray[0]);
-    $("#card2Face").addClass(shuffledArray[1]);
-    $("#card3Face").addClass(shuffledArray[2]);
-    $("#card4Face").addClass(shuffledArray[3]);
-    $("#card5Face").addClass(shuffledArray[4]);
-    $("#card6Face").addClass(shuffledArray[5]);
-    $("#card7Face").addClass(shuffledArray[6]);
-    $("#card8Face").addClass(shuffledArray[7]);
-    $("#card9Face").addClass(shuffledArray[8]);
-    $("#card10Face").addClass(shuffledArray[9]);
-    $("#card11Face").addClass(shuffledArray[10]);
-    $("#card12Face").addClass(shuffledArray[11]);
-    $("#card1").addClass(`${shuffledArray[0]}A`);
-    $("#card2").addClass(`${shuffledArray[1]}A`);
-    $("#card3").addClass(`${shuffledArray[2]}A`)
-    $("#card4").addClass(`${shuffledArray[3]}A`);
-    $("#card5").addClass(`${shuffledArray[4]}A`);
-    $("#card6").addClass(`${shuffledArray[5]}A`);
-    $("#card7").addClass(`${shuffledArray[6]}A`);
-    $("#card8").addClass(`${shuffledArray[7]}A`);
-    $("#card9").addClass(`${shuffledArray[8]}A`);
-    $("#card10").addClass(`${shuffledArray[9]}A`);
-    $("#card11").addClass(`${shuffledArray[10]}A`);
-    $("#card12").addClass(`${shuffledArray[11]}A`);
+    // //End of tutorial code
+    // console.log("this is the shuffled array " + shuffledArray);
+    // //for loop all of these.
+    // $("#card1Face").addClass(shuffledArray[0]);
+    // $("#card2Face").addClass(shuffledArray[1]);
+    // $("#card3Face").addClass(shuffledArray[2]);
+    // $("#card4Face").addClass(shuffledArray[3]);
+    // $("#card5Face").addClass(shuffledArray[4]);
+    // $("#card6Face").addClass(shuffledArray[5]);
+    // $("#card7Face").addClass(shuffledArray[6]);
+    // $("#card8Face").addClass(shuffledArray[7]);
+    // $("#card9Face").addClass(shuffledArray[8]);
+    // $("#card10Face").addClass(shuffledArray[9]);
+    // $("#card11Face").addClass(shuffledArray[10]);
+    // $("#card12Face").addClass(shuffledArray[11]);
+    // $("#card1").addClass(`${shuffledArray[0]}A`);
+    // $("#card2").addClass(`${shuffledArray[1]}A`);
+    // $("#card3").addClass(`${shuffledArray[2]}A`)
+    // $("#card4").addClass(`${shuffledArray[3]}A`);
+    // $("#card5").addClass(`${shuffledArray[4]}A`);
+    // $("#card6").addClass(`${shuffledArray[5]}A`);
+    // $("#card7").addClass(`${shuffledArray[6]}A`);
+    // $("#card8").addClass(`${shuffledArray[7]}A`);
+    // $("#card9").addClass(`${shuffledArray[8]}A`);
+    // $("#card10").addClass(`${shuffledArray[9]}A`);
+    // $("#card11").addClass(`${shuffledArray[10]}A`);
+    // $("#card12").addClass(`${shuffledArray[11]}A`);
     // var formattedShuffledArray = "'" + shuffledArray[0] + shuffledArray[1] + shuffledArray[2] + "'"
     //                             + "'" + shuffledArray[3] +shuffledArray[4] + shuffledArray[5] + "'" 
     //                             + "'" + shuffledArray[6] + shuffledArray[7] +shuffledArray[8] + "'"
