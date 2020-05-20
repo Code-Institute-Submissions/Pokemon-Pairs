b2822823ef2df447f77867be71ff01916708845a
Chris Palmer <cgpalmer91@gmail.com>
2020-05-20 22:30:08 +0000

    Refactoring code and commenting out old code.

--- assets/js/game.js
+++ assets/js/game.js
@@ -154,153 +154,124 @@ function notAPair(){
 }
 
 
-$('#gameRow').on('click', '.cardMatchID1', function(){ 
-    movesTaken++
-    console.log(movesTaken);
-                if (pairCounterArray[1] == 1){
-                    pairCounterArray[1] = 2;
-                    pairMatch++;
-                    console.log(pairCounterArray[1]);
-                    } else {
+// $('#gameRow').on('click', '.cardMatchID1', function(){ 
+//     movesTaken++
+//     console.log(movesTaken);
+//                 if (pairCounterArray[1] == 1){
+//                     pairCounterArray[1] = 2;
+//                     pairMatch++;
+//                     console.log(pairCounterArray[1]);
+//                     } else {
                         
-                        if ( pairCounterArray[2] == 1 || pairCounterArray[3] == 1 || pairCounterArray[4] == 1 
-                        || pairCounterArray[5] == 1 || pairCounterArray[6] == 1){  
-                        notAPair();
-                        }else {
-                        pairCounterArray[1] = 1;
-                        $(this).removeClass('cardMatchID1');
-                    }
-                    }
-                });
+//                         if ( pairCounterArray[2] == 1 || pairCounterArray[3] == 1 || pairCounterArray[4] == 1 
+//                         || pairCounterArray[5] == 1 || pairCounterArray[6] == 1){  
+//                         notAPair();
+//                         }else {
+//                         pairCounterArray[1] = 1;
+//                         $(this).removeClass('cardMatchID1');
+//                     }
+//                     }
+//                 });
 
-$('#gameRow').on('click', '.cardMatchID2', function(){ 
-    movesTaken++
-    console.log(movesTaken);
-                if (pairCounterArray[2] == 1){
-                    pairCounterArray[2] = 2;
-                    pairMatch++;
-                    console.log(pairCounterArray[2]);
-                    } else {
+// $('#gameRow').on('click', '.cardMatchID2', function(){ 
+//     movesTaken++
+//     console.log(movesTaken);
+//                 if (pairCounterArray[2] == 1){
+//                     pairCounterArray[2] = 2;
+//                     pairMatch++;
+//                     console.log(pairCounterArray[2]);
+//                     } else {
                        
-                        if ( pairCounterArray[5] == 1 || pairCounterArray[3] == 1 || pairCounterArray[1] == 1 
-                            || pairCounterArray[4] == 1 || pairCounterArray[6] == 1){
-                                notAPair();
-                            }else {
-                        pairCounterArray[2] = 1;
-                        $(this).removeClass('cardMatchID2');
-                    }
-                    }
-                });
+//                         if ( pairCounterArray[5] == 1 || pairCounterArray[3] == 1 || pairCounterArray[1] == 1 
+//                             || pairCounterArray[4] == 1 || pairCounterArray[6] == 1){
+//                                 notAPair();
+//                             }else {
+//                         pairCounterArray[2] = 1;
+//                         $(this).removeClass('cardMatchID2');
+//                     }
+//                     }
+//                 });
 
-$('#gameRow').on('click', '.cardMatchID3', function(){ 
-    movesTaken++
-    console.log(movesTaken);
-                if (pairCounterArray[3] == 1){
-                    pairCounterArray[3] = 2;
-                    pairMatch++;
-                    console.log(pairCounterArray[3]);
-                    } else {
+// $('#gameRow').on('click', '.cardMatchID3', function(){ 
+//     movesTaken++
+//     console.log(movesTaken);
+//                 if (pairCounterArray[3] == 1){
+//                     pairCounterArray[3] = 2;
+//                     pairMatch++;
+//                     console.log(pairCounterArray[3]);
+//                     } else {
                        
-                        if ( pairCounterArray[2] == 1 || pairCounterArray[5] == 1 || pairCounterArray[1] == 1 
-                            || pairCounterArray[4] == 1 || pairCounterArray[6] == 1){
-                                notAPair();
-                            }else {
-                        pairCounterArray[3] = 1;
-                        $(this).removeClass('cardMatchID3');
-                    }
-                    }
-                });
+//                         if ( pairCounterArray[2] == 1 || pairCounterArray[5] == 1 || pairCounterArray[1] == 1 
+//                             || pairCounterArray[4] == 1 || pairCounterArray[6] == 1){
+//                                 notAPair();
+//                             }else {
+//                         pairCounterArray[3] = 1;
+//                         $(this).removeClass('cardMatchID3');
+//                     }
+//                     }
+//                 });
 
-$('#gameRow').on('click', '.cardMatchID4', function(){ 
-    movesTaken++
-    console.log(movesTaken);
-                if (pairCounterArray[4] == 1){
-                    pairCounterArray[4] = 2;
-                    pairMatch++;
-                    console.log(pairCounterArray[4]);
-                    } else {
+// $('#gameRow').on('click', '.cardMatchID4', function(){ 
+//     movesTaken++
+//     console.log(movesTaken);
+//                 if (pairCounterArray[4] == 1){
+//                     pairCounterArray[4] = 2;
+//                     pairMatch++;
+//                     console.log(pairCounterArray[4]);
+//                     } else {
                     
                         
-                        if ( pairCounterArray[2] == 1 || pairCounterArray[3] == 1 || pairCounterArray[1] == 1 
-                            || pairCounterArray[5] == 1 || pairCounterArray[6] == 1){
-                                notAPair();
-                            }else {
-                        pairCounterArray[4] = 1;
-                        $(this).removeClass('cardMatchID4');
-                    }
-                    }
-                });                
+//                         if ( pairCounterArray[2] == 1 || pairCounterArray[3] == 1 || pairCounterArray[1] == 1 
+//                             || pairCounterArray[5] == 1 || pairCounterArray[6] == 1){
+//                                 notAPair();
+//                             }else {
+//                         pairCounterArray[4] = 1;
+//                         $(this).removeClass('cardMatchID4');
+//                     }
+//                     }
+//                 });                
 
 
 
-$('#gameRow').on('click', '.cardMatchID5', function(){ 
-movesTaken++
-    console.log(movesTaken);
-                if (pairCounterArray[5] == 1){
-                    pairCounterArray[5] = 2;
-                    pairMatch++;
-                    console.log(pairCounterArray[5]);
-                    } else {
+// $('#gameRow').on('click', '.cardMatchID5', function(){ 
+// movesTaken++
+//     console.log(movesTaken);
+//                 if (pairCounterArray[5] == 1){
+//                     pairCounterArray[5] = 2;
+//                     pairMatch++;
+//                     console.log(pairCounterArray[5]);
+//                     } else {
                        
-                        if ( pairCounterArray[2] == 1 || pairCounterArray[3] == 1 || pairCounterArray[1] == 1 
-                            || pairCounterArray[4] == 1 || pairCounterArray[6] == 1){
-                                notAPair();
-                            }else {
-                        pairCounterArray[5] = 1;
-                        $(this).removeClass('cardMatchID5');
-                    }
-                    }
-                });
+//                         if ( pairCounterArray[2] == 1 || pairCounterArray[3] == 1 || pairCounterArray[1] == 1 
+//                             || pairCounterArray[4] == 1 || pairCounterArray[6] == 1){
+//                                 notAPair();
+//                             }else {
+//                         pairCounterArray[5] = 1;
+//                         $(this).removeClass('cardMatchID5');
+//                     }
+//                     }
+//                 });
 
 
-$('#gameRow').on('click', '.cardMatchID6', function(){
-                    movesTaken++
-                    console.log(movesTaken);
-                    if (pairCounterArray[6] == 1){
-                    pairCounterArray[6] = 2;
-                    pairMatch++;
-                    console.log(pairCounterArray[6]);
-                    } else {
+// $('#gameRow').on('click', '.cardMatchID6', function(){
+//                     movesTaken++
+//                     console.log(movesTaken);
+//                     if (pairCounterArray[6] == 1){
+//                     pairCounterArray[6] = 2;
+//                     pairMatch++;
+//                     console.log(pairCounterArray[6]);
+//                     } else {
                         
-                        if ( pairCounterArray[1] == 1 || pairCounterArray[2] == 1 || pairCounterArray[3] == 1 || pairCounterArray[4] == 1 
-                        || pairCounterArray[5] == 1 || pairCounterArray[6] == 1){
-                        notAPair();
-                    } else {
-                        pairCounterArray[6] = 1;
-                        $(this).removeClass('cardMatchID6');
-                    }
-                }
+//                         if ( pairCounterArray[1] == 1 || pairCounterArray[2] == 1 || pairCounterArray[3] == 1 || pairCounterArray[4] == 1 
+//                         || pairCounterArray[5] == 1 || pairCounterArray[6] == 1){
+//                         notAPair();
+//                     } else {
+//                         pairCounterArray[6] = 1;
+//                         $(this).removeClass('cardMatchID6');
+//                     }
+//                 }
             
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// });
 
 
 
