8d41b5d2f0ad95cc7cc9089916d1c522829f83d9
Chris Palmer <cgpalmer91@gmail.com>
2020-05-25 13:13:25 +0000

    Repeating the tests for all 12 cards.

--- assets/js/specsJS/gamesSpec.js
+++ assets/js/specsJS/gamesSpec.js
@@ -15,4 +15,49 @@ describe("Give an ID based on the card selected", function() {
             var result = clickedCard(1)
             expect(result).toBe("#card1");
         });
+    it("should return as #card2 when called as clickedCard(2)", function() {
+            var result = clickedCard(2)
+            expect(result).toBe("#card2");
+        });
+
+         it("should return as #card3 when called as clickedCard(3)", function() {
+            var result = clickedCard(3)
+            expect(result).toBe("#card3");
+        });
+         it("should return as #card4 when called as clickedCard(4)", function() {
+            var result = clickedCard(4)
+            expect(result).toBe("#card4");
+        });
+         it("should return as #card5 when called as clickedCard(5)", function() {
+            var result = clickedCard(5)
+            expect(result).toBe("#card5");
+        });
+         it("should return as #card6 when called as clickedCard(6)", function() {
+            var result = clickedCard(6)
+            expect(result).toBe("#card6");
+        });
+         it("should return as #card7 when called as clickedCard(7)", function() {
+            var result = clickedCard(7)
+            expect(result).toBe("#card7");
+        });
+         it("should return as #card8 when called as clickedCard(8)", function() {
+            var result = clickedCard(8)
+            expect(result).toBe("#card8");
+        });
+         it("should return as #card9 when called as clickedCard(9)", function() {
+            var result = clickedCard(9)
+            expect(result).toBe("#card9");
+             });
+             it("should return as #card10 when called as clickedCard(10)", function() {
+            var result = clickedCard(10)
+            expect(result).toBe("#card10");
+        }); it("should return as #card11 when called as clickedCard(11)", function() {
+            var result = clickedCard(11)
+            expect(result).toBe("#card11");
+        }); 
+        it("should return as #card12 when called as clickedCard(12)", function() {
+            var result = clickedCard(12)
+            expect(result).toBe("#card12");
+        });
+         
 });
