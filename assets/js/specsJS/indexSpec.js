196665c0b242632ae124609c2bf894f671f672ef
Chris Palmer <cgpalmer91@gmail.com>
2020-05-25 13:25:31 +0000

    Adding in the difficulty for #easy to test.

--- assets/js/specsJS/indexSpec.js
+++ assets/js/specsJS/indexSpec.js
@@ -1,7 +1,12 @@
-describe("Deciding which difficult to make active", function() {
+describe("Deciding which difficulty to make active", function() {
    
     it("should exist", function() {
             expect(selectingActiveDifficulty).toBeDefined();
+        });
+
+    it("should select #easy if the code is 1", function() {
+            var result = selectingActiveDifficulty(1)
+            expect(result).toBe("#easy");
         });
 
     });
