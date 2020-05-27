bdc48cb6a3a45da3b1e2aaf62e9f8c891559a5c9
Chris Palmer <cgpalmer91@gmail.com>
2020-05-27 16:49:24 +0000

    Adding in a final test and removing comments.

--- assets/js/specsJS/gamesSpec.js
+++ assets/js/specsJS/gamesSpec.js
@@ -5,9 +5,6 @@ describe("Give an ID based on the card selected", function() {
     });
 
     it("should exist", function() {
-            // Expectations are built with the function expect which takes a value, called the actual.
-            // Each matcher implements a boolean comparison between the actual value and the expected value.
-            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
             expect(clickedCard).toBeDefined();
         });
 
--- assets/js/specsJS/indexSpec.js
+++ assets/js/specsJS/indexSpec.js
@@ -5,9 +5,6 @@ describe("Show which theme song has been chosen", function() {
     });
 
     it("should exist", function() {
-            // Expectations are built with the function expect which takes a value, called the actual.
-            // Each matcher implements a boolean comparison between the actual value and the expected value.
-            // Any matcher can evaluate to a negative assertion by chaining the call to expect with a not before calling the matcher.
             expect(activeTheme).toBeDefined();
         });
 
@@ -26,10 +23,46 @@ describe("Show which theme song has been chosen", function() {
             expect(result).toBe(gameTheme);
         });
 
-    it("should return as gameTheme when themeSelector is 2", function() {
+    it("should return as endTheme when themeSelector is 3", function() {
             var result = activeTheme(3)
             expect(result).toBe(endTheme);
         });
     
+});
+
+
+
+
+describe("Show which theme modal is currently active", function() {
+    
+    beforeEach(function() {
+        card = new modalSelectionFunction(whichModal);
+    });
+
+    it("should exist", function() {
+            expect(modalSelectionFunction).toBeDefined();
+        });
+
+    it("should return as volumeModal when whichModal is 1", function() {
+            var result = modalSelectionFunction(1)
+            expect(result).toBe("#volumeModal");
+        });
+
+    it("should return as welcomeModal when whichModal is 2", function() {
+            var result = modalSelectionFunction(2)
+            expect(result).toBe("#welcomeModal");
+        });
+
+    it("should return as difficultyModal when whichModal is 3", function() {
+            var result = modalSelectionFunction(3)
+            expect(result).toBe("#difficultyModal");
+        });
+
+    it("should return as finishedModal when whichModal is 4", function() {
+            var result = modalSelectionFunction(4)
+            expect(result).toBe("#finishedModal");
+        });
+
+   
 });
 
